9ab965739ea8530f2c2463aa733c9fe653ad3f65
Marcus Kida <mail@marcuskida.de>
2013-09-26 09:54:50 +0200

    Implement JavaScriptCore sample using JS add method

--- JavaScriptCoreSample/KIViewController.m
+++ JavaScriptCoreSample/KIViewController.m
@@ -9,7 +9,12 @@
 #import "KIViewController.h"
 
 @interface KIViewController ()
-
+{
+    JSContext *_jsContext;
+    
+    IBOutlet UITextView *_inTextView;
+    IBOutlet UITextView *_outTextView;
+}
 @end
 
 @implementation KIViewController
@@ -18,12 +23,29 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [_inTextView setText:@"function add(one, two) {\n   return one + two;\n}\nresult = add(3, 7);"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    _jsContext = [[JSContext alloc] initWithVirtualMachine:[[JSVirtualMachine alloc] init]];
+    _jsContext[@"result"] = @"";
+}
+
+- (IBAction)interpret:(id)sender
+{
+    [_jsContext evaluateScript:[_inTextView text]];
+    
+    JSValue *val = _jsContext[@"result"];
+    [_outTextView setText:[val toString]];
 }
 
 @end
